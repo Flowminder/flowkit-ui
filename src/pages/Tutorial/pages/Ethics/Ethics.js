c52221ee14ab63cc33c189fc2af61e0bcfd2cf8e
Stef <stefanie.wiegand@flowminder.org>
2023-02-23 16:38:51 +0000

    [wip] added ethics page

--- src/pages/Tutorial/pages/Ethics/Ethics.js
+++ src/pages/Tutorial/pages/Ethics/Ethics.js
@@ -1,5 +1,9 @@
 import React from "react"
 import styles from "./Ethics.module.css"
+import mainStyles from "../../../../components/MainContent/MainContent.module.css"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import { useTranslation } from "react-i18next"
 import { FMTrans } from "../../../../components"
 
@@ -8,13 +12,35 @@ const Ethics = () => {
 
     return (
         <div className={styles.Ethics} data-testid="Ethics">
-            <h2>{t("tutorial.terms")}</h2>
+            <Container className={`${mainStyles.fullWidth} ${mainStyles.pageNav}`}>
+                <Row>
+                    <Col>
+                        <h2>{t("tutorial.sections_links")}</h2>
+                        <ul>
+                            <li>
+                                <a href="#ethics_heading1">{t("tutorial.ethics_heading1")}</a>
+                            </li>
+                            <li>
+                                <a href="#ethics_heading2">{t("tutorial.ethics_heading2")}</a>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
+            <br />
 
-            <h3>
-                <FMTrans k="tutorial.heading5" />
-            </h3>
+            <h2 id="ethics_heading1">
+                <FMTrans k="tutorial.ethics_heading1" />
+            </h2>
             <p>
-                <FMTrans k="tutorial.text5" />
+                <FMTrans k="tutorial.ethics_text1" />
+            </p>
+            <br />
+            <h2 id="ethics_heading2">
+                <FMTrans k="tutorial.ethics_heading2" />
+            </h2>
+            <p>
+                <FMTrans k="tutorial.ethics_text2" />
             </p>
         </div>
     )
