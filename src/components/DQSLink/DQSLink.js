971f7a9f4108fbb04feb7c1b09cdf5546be6af54
Thingus <john.roberts@flowminder.org>
2025-04-25 15:25:18 +0100

    Error catch and MPL header

--- src/components/DQSLink/DQSLink.js
+++ src/components/DQSLink/DQSLink.js
@@ -1,10 +1,12 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import { useTranslation } from "react-i18next"
 import { useSelector, useDispatch } from "react-redux"
 import { useAuth0 } from "@auth0/auth0-react"
 import { Link } from "react-router-dom"
 import api from "../../app/api"
 import session from "../SessionArea/sessionSlice.selectors"
-import FMButton from "../FMButton/FMButton"
 
 const DQSLink = () => {
     const { t } = useTranslation()
@@ -14,12 +16,16 @@ const DQSLink = () => {
     const downloadDQS = async () => {
         console.log("Fetching signed link....")
         const dqs_link = await api.dqs(auth0AccessToken)
-        console.log(`Got signed link: ${dqs_link.url}`)
-        const element = document.createElement("a")
-        element.href = dqs_link.url
-        element.download = dqs_link.file_name
-        element.click()
-        element.remove()
+        if (dqs_link) {
+            console.log(`Got signed link: ${dqs_link.url}`)
+            const element = document.createElement("a")
+            element.href = dqs_link.url
+            element.download = dqs_link.file_name
+            element.click()
+            element.remove()
+        } else {
+            console.error("DQS download error, please contact admin")
+        }
     }
 
     if (isAuthenticated) {
